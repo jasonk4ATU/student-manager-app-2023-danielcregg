dfe28cc519e9fff973ea19f7a4a9597b7c2f373d
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-10-09 14:37:23 +0000

    Add stuff to student manager

--- ie/atu/intro/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package ie.atu.intro;
-
-// This program prints 'Hello, World!' to the console.
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Prints 'Hello, World!' to the console.
-        System.out.println("Hello, World!");
-    }
-}
--- ie/atu/studentmanagerpackage/StudentManager.java
+++ ie/atu/studentmanagerpackage/StudentManager.java
@@ -1,7 +1,43 @@
 package ie.atu.studentmanagerpackage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentManager {
-    // Add Student
-    // Delete Student
-    // Update Student Details
+
+    // Create an arrayList to hold Student objects
+    List<Student> studentArrayList = new ArrayList<Student>();
+
+    // Add a student to the arrayList
+    public void addStudent(Student student) {
+        studentArrayList.add(student);
+    }
+
+    // Remove a student from the arrayList
+    public void removeStudent(Student student) {
+        studentArrayList.remove(student);
+    }
+
+    // Find the total number of students in the arrayList
+    public int findTotalStudents() {
+        return studentArrayList.size();
+    }
+
+    // Search for a student by studentId
+    public Student findStudentById(String studentId) {
+        for (Student student : studentArrayList) {
+            if (student.getStudentId().equals(studentId)) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    // Print instance variables a given student object
+    public void printStudent(Student student) {
+        System.out.println("Student ID: " + student.getStudentId());
+        System.out.println("Student Name: " + student.getName());
+        System.out.println("Student Age: " + student.getAge());
+    }
+   
 }
