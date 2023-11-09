bac195f939a272472d64c46ab12fe73d4a7462f0
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-11-09 15:27:54 +0000

    Refactored readStudentDataFromCSVFile method in Main and StudentManager classes

--- ie/atu/studentmanagerpackage/Main.java
+++ ie/atu/studentmanagerpackage/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Create a StudentManager object
         StudentManager studentManager = new StudentManager();
         // Read student data from a csv file and add to the StudentManager object arrayList
-        studentManager.readStudentDataFromCSVFile2("./resources/students.csv");
+        studentManager.readStudentDataFromCSVFile("./resources/students.csv");
         // Find total number of students in the StudentManager object arrayList
         System.out.println("Total number of students: " + studentManager.findTotalStudents());
         // Print all students in the StudentManager object arrayList
